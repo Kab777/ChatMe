c4ac41229ce3bbdcb351fe156f4f76e1f5664f1b
Junyu <adbcsf19@gmail.com>
2016-10-09 01:20:00 -0400

    added java files

---
+++ app/src/main/java/com/junyu/moneymanager/MainActivity.java
@@ -0,0 +1,29 @@
+package com.junyu.moneymanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import timber.log.Timber;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.testButton) Button testButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+    }
+}
