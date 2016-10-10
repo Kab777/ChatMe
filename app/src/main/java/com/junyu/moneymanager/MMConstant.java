312e1e8666db8cbe016869c9e3436e710fbc35e2
Junyu <adbcsf19@gmail.com>
2016-10-10 01:55:33 -0400

    added group home page

--- app/src/main/java/com/junyu/moneymanager/MMConstant.java
+++ app/src/main/java/com/junyu/moneymanager/MMConstant.java
@@ -6,8 +6,12 @@ package com.junyu.moneymanager;
 
 public class MMConstant {
     public static final String USERS = "Users";
+    public static final String Groups = "Groups";
     public static final String USER_ID = "userId";
     public static final String GROUP_ID = "groupId";
     public static final String NAME = "name";
     public static final String PREFERENCE_NAME = "userInfo";
+    public static final String BALANCE = "balance";
+    public static final String GROUPNAME = "groupName";
+    public static final String MEMBERS = "members";
 }
