f67fe98b2a25ec5529f6ada662d27bf92b0ca0ae
wanjie <wanjiezhang.cathy@gmail.com>
2016-10-10 20:08:55 -0400

    solver merge conflict, transaction function

--- app/src/main/java/com/junyu/moneymanager/MainActivity.java
+++ app/src/main/java/com/junyu/moneymanager/MainActivity.java
@@ -22,6 +22,7 @@ import com.google.firebase.database.DatabaseError;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Transaction;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.HashMap;
